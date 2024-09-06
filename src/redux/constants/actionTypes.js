efec11e4f40ee3a0e1ecad94c3991bc98639165b
08Uzer <121750611+08Uzair@users.noreply.github.com>
2024-09-06 09:21:07 +0530

    Assignment Completed

---
+++ src/redux/constants/actionTypes.js
@@ -0,0 +1,8 @@
+export const FETCH_USERS = "FETCH_USERS";
+export const CREATE_USERS = "FETCH_USERS";
+export const DELETE_USER = "DELETE_USER ";
+export const UPDATE_USER = "UPDATE_USER ";
+export const FETCH_USER_ID = "FETCH_USER_ID";
+export const SEARCH_USER = "SEARCH_USER";
+
+ 
